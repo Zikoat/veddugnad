239ae65edf96023342da9ff1197957795482f21f
prompemannen <sschoeler99@gmail.com>
2024-02-18 15:52:05 +0100

    Pull changes and push tag.

--- veddugnad.bat
+++ veddugnad.bat
@@ -1,3 +1,12 @@
 @echo off
-.\venv\scripts\python c:\veddugnad\veddugnad.py %*
+cd /d "%~dp0"
+echo Pulling latest changes from the main branch...
+git checkout main
+git pull
+echo Creating/Updating the 'prod' tag...
+git tag -f prod
+git push origin :refs/tags/prod
+git push origin prod
+echo Update complete.
+.\venv\Scripts\python .\veddugnad.py %*
 pause
